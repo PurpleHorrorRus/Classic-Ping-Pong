11bb59edd20467a8d6e22ea334a8272abe137468
Maxim <zenit77798@yandex.ru>
2016-06-02 21:16:35 +0800

    Code improvements

--- src/Game.java
+++ src/Game.java
@@ -1,10 +1,18 @@
+package Main;
+
+import java.io.File;
+
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 
+import jaco.mp3.player.MP3Player;
+
 public class Game {
 	
 	static boolean started;
+	
+	public static MP3Player mp;
 	
 	public static void start(){
 		started = true;
@@ -21,8 +29,8 @@ public class Game {
 		Ball.move.stop();
 		Ball.ping = false;
 		Ball.pong = false;
-		Ball.x = (int) Player.getX()+50;
-		Ball.y = (int) Player.getY()+80;
+		Ball.x = (int) Player.getX(0)+50;
+		Ball.y = (int) Player.getY(0)+80;
 	}
 	
 	public static synchronized void playSound(final String url) {
@@ -31,11 +39,16 @@ public class Game {
 				try {
 					Clip clip = AudioSystem.getClip();
 					AudioInputStream stream = AudioSystem.getAudioInputStream(
-					ClassicPong.class.getResourceAsStream("/sounds/" + url));
+					ClassicPong.class.getResource("/sounds/" + url));
 					clip.open(stream);
 					clip.start(); 
 				} catch (Exception e) { }
 			}
 		}).start();
+	}
+	
+	public static void playMusic(String src){
+		if(mp == null) { mp = new MP3Player(new File("src/sounds/"+src)); mp.play(); mp.setRepeat(true); }
+		else { mp.stop(); mp = null; }
 	}
 }
