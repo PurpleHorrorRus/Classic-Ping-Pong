8b4611392f0fdebef1712a85aa3b5dd8a2a6a8fa
Maxim <zenit77798@yandex.ru>
2016-06-02 21:15:19 +0800

    Code improvements

--- src/Player.java
+++ src/Player.java
@@ -1,18 +1,29 @@
+package Main;
+
 public class Player {
-	//Initial coordinates
-	static double x = 0.0; 
-	static double y = 165.0;
+	static double[] x = new double[2];
+	static double[] y = new double[2];
 	
-	public static void setPos(double x, double y){
-		Player.x = x;
-		Player.y = y;
+	static boolean spell[] = new boolean[3];
+	
+	public static void init(){
+		x[0] = 0.0;
+		y[0] = 165.0;
+		
+		x[1] = 745.0;
+		y[1] = 165.0;
 	}
 	
-	public static double getX(){
-		return Player.x;
+	public static void setPos(int playerid, double x, double y){
+		Player.x[playerid] = x;
+		Player.y[playerid] = y;
 	}
 	
-	public static double getY(){
-		return Player.y;
+	public static double getX(int playerid){
+		return Player.x[playerid];
+	}
+	
+	public static double getY(int playerid){
+		return Player.y[playerid];
 	}
 }
