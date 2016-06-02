a652c518be8bd8cf38363769b6f43338e0ff6498
Maxim <zenit77798@yandex.ru>
2016-06-02 21:18:45 +0800

    Code improvements

--- src/Frame.java
+++ src/Frame.java
@@ -1,3 +1,5 @@
+package Main;
+
 import java.awt.Rectangle;
 import java.awt.Toolkit;
 
@@ -11,15 +13,15 @@ public class Frame extends JFrame {
 	Frame(){
 		setResizable(false);
 		setSize(800, 600);
-		setTitle("Classic Ping-Pong");
+		setTitle("Classic Ping-Pong [Nikifor0ff.ru]");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		Render.playerRectangle = new Rectangle(0, 0, 50, 200);
-		Render.player2Rectangle = new Rectangle(730, 100, 50, 200);
-		Render.ball = new Rectangle(0, 0, 50, 50);
+		Render.playerRectangle[0] = new Rectangle(0, 0, 50, 200);
+		Render.playerRectangle[1] = new Rectangle(730, 100, 50, 200);
+		Render.ball = new Rectangle(0, 0, Ball.getSizeX(), Ball.getSizeY());
 		GameWindow = new Render();
 		add(GameWindow);
 		addKeyListener(new Keyboard());
-		setIconImage(Toolkit.getDefaultToolkit().getImage("icon.png"));
+		setIconImage(Toolkit.getDefaultToolkit().getImage(getClass().getResource("icon.png")));
 		setLocationRelativeTo(null);
 		setVisible(true);
 	}
