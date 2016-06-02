f5fedab39b4a6cf44b28f3cd9582d7b1267d5a42
Maxim <zenit77798@yandex.ru>
2016-06-02 21:19:05 +0800

    Code improvements

--- src/Render.java
+++ src/Render.java
@@ -7,17 +7,17 @@ import javax.swing.JPanel;
 
 public class Render extends JPanel {
 		
-	static Rectangle playerRectangle, player2Rectangle, ball;
+	static Rectangle[] playerRectangle = new Rectangle[2];
+	static Rectangle ball;
 	
 	@Override
 	protected void paintComponent(java.awt.Graphics g) {
 		super.paintComponent(g);
-		
 		g.setColor(Color.BLACK);
 		g.fillRect(0, 0, getWidth(), getHeight());
 						
-		playerRectangle.setLocation((int)Player.getX(0), (int)Player.getY(0));
-		player2Rectangle.setLocation((int)Player.getX(1), (int)Player.getY(1));
+		playerRectangle[0].setLocation((int)Player.getX(0), (int)Player.getY(0));
+		playerRectangle[1].setLocation((int)Player.getX(1), (int)Player.getY(1));
 		g.setColor(Color.WHITE);
 		g.fillRect((int) Player.getX(0), (int) Player.getY(0), 50, 200);
 		g.fillRect((int) Player.getX(1), (int) Player.getY(1), 50, 200);
@@ -30,8 +30,8 @@ public class Render extends JPanel {
 		}
 		
 		else { g.fillOval(Ball.getBallX(), Ball.getBallY(), Ball.getSizeX(), Ball.getSizeY()); 
-			ball.setLocation(Ball.getBallX(), Ball.getBallY());
-			ball.setSize(Ball.getSizeX(), Ball.getSizeY());
+		ball.setLocation(Ball.getBallX(), Ball.getBallY());
+		ball.setSize(Ball.getSizeX(), Ball.getSizeY());
 		}
 		
 		Keyboard.tick();
